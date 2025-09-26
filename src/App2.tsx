0dd24006e1e9b61e8ae3e3de173caa2875eae6e8
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2025-09-26 09:30:44 +0300

    64. Generics, state, props

---
+++ src/App2.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './App2.css';
+
+type ChildProps = {
+	parentCounter: number;
+};
+
+function ChildCounter(props: ChildProps) {
+	const [childCounter, setChildCounter] = useState<number>(0);
+	function handleClickIncrement() {
+		setChildCounter(childCounter + 1);
+	}
+	console.log('Rendering child component');
+	return (
+		<div style={{ border: '1px dashed red' }}>
+			<button onClick={handleClickIncrement}>Increment child counter</button>
+			<p>ChildCounter: {childCounter}</p>
+			<p>ParentCounter: {props.parentCounter}</p>
+		</div>
+	);
+}
+
+function ParentCounter() {
+	const [parentCounter, setParentCounter] = useState<number>(0);
+	function handleClickIncrement() {
+		setParentCounter(parentCounter + 1);
+	}
+	console.log('Rendering parent component');
+	return (
+		<div className='parent' style={{ border: '1px dashed blue' }}>
+			<button onClick={handleClickIncrement}>Increment parent counter</button>
+			<ChildCounter parentCounter={parentCounter} />
+		</div>
+	);
+}
+
+function App2() {
+	return (
+		<div className='App'>
+			App works!
+			<br />
+			<ParentCounter />
+		</div>
+	);
+}
+
+export default App2;
