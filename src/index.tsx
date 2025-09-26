0fc85789075b88a47bb4c64560d5a2dbc94a41ec
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2025-09-26 10:04:33 +0300

    65. Add LoginComponent / LoginService

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,16 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client';
+import LoginComponent from './LoginComponent';
+import LoginService from './services/LoginService';
 import './index.css';
-// import App from './App';
-import App2 from './App2';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
-root.render(
-	// <React.StrictMode>
-	<App2 />
-	// </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+const loginService = new LoginService();
+
+const setToken = (token: string) => {
+	console.log(`received the token ${token}`);
+};
+
+root.render(<LoginComponent loginService={loginService} setToken={setToken} />);
